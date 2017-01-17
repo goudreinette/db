ddb2fe55ad9808175b114ff9145c9cd0ad4f6425
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-17 11:50:54 +0100

    -

--- src/date.clj
+++ src/date.clj
@@ -1,21 +1,20 @@
 (ns date
   (:refer-clojure :exclude [format])
-  (:require [hara.time :refer [now to-map plus minus before adjust format coerce]]
+  (:require [hara.time :refer [now to-map default-type plus minus before adjust format coerce]]
             [clojure.set :refer [map-invert]]))
 
-(defn as-date [date-like]
-  (coerce date-like {:type java.util.Date}))
+(default-type java.util.Date)
+
 
 (defn format-date [date]
   (format date "E dd-MM-yyy HH:ss"))
 
 (defn date-range [from to by]
   (take-while #(before % to)
-               (iterate #(as-date (plus % by)) from)))
+               (iterate #(plus % by) from)))
 
 (defn at-or-rewind [& {:keys [rewind at]}]
-  (as-date
-    (cond
-      at     (adjust (now) at)
-      rewind (minus  (now) (map-invert rewind))
-      :else  (now))))
+  (cond
+    at     (adjust (now) at)
+    rewind (minus  (now) (map-invert rewind))
+    :else  (now)))
