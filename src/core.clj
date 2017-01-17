5a4b8f481bbccf4d5d5a9c63b06833f78c5f2e90
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-17 11:43:12 +0100

    states

--- src/core.clj
+++ src/core.clj
@@ -2,7 +2,7 @@
   (:use date set)
   (:refer-clojure :exclude [find])
   (:require [clojure.core.match :refer [match]]
-            [hara.time :refer [now before minus adjust from-map]]))
+            [hara.time :refer [now before after minus adjust from-map]]))
 
 (defrecord DB [file history state])
 (defrecord Event [type attributes date])
@@ -21,13 +21,12 @@
       [{:type :retract :attributes a          }] (remove-attrs-everywhere a all) ; or all?
       [{:type :retract               :where w }] (remove-where w all))))
 
-(defn replay
-  ([history]
-   (replay history false))
-  ([history states?]
-   (cond->> history
-     states?       (reductions transition #{})
-     (not states?) (reduce transition #{}))))
+
+(defn replay [history]
+  (reduce transition #{} history))
+
+(defn replay-reductions [history]
+  (reductions transition #{} history))
 
 (defn exec-event [type {:as db :keys [history state]} attributes attrs]
   (as-> (apply event type attributes attrs) event
@@ -35,13 +34,17 @@
       :history (conj history event)
       :state   (transition state event))))
 
+(defn history-until-date [history to-date]
+  (take-while #(before (:date %) to-date) history))
 
 ; Pure API
 
+
 (defn rewind [history to-date]
-  (->> history
-    (take-while #(before (:date %) to-date))
+  (-> history
+    (history-until-date to-date)
     (replay)))
+
 
 (defn find [{h :history} {a :at r :rewind w :where p :project}]
   (->>
@@ -55,6 +58,11 @@
         results (map #(find db (assoc options :at %) dates))]
     (map vector dates results)))
 
+(defn states [history to-date]
+  (let [history (history-until-date history to-date)]
+    (interleave (map :date history)
+                (replay-reductions history))))
+
 
 ; Persistence
 (defn init [file]
@@ -64,3 +72,6 @@
 
 (defn save [db]
   (spit (db :file) (db :history)))
+
+; Test
+(def db (init "db.edn"))
