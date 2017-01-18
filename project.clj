1660750a0ee30cd9b8be40b7260fa193929a5f92
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-18 13:46:37 +0100

    sandbox dependency, group prefix

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject db "0.0.1"
+(defproject reinvdwoerd/db "0.0.1"
   :description ""
   :dependencies
     [[org.clojure/clojure "1.8.0"]
      [org.clojure/core.match "0.3.0-alpha4"]
      [org.clojure/core.incubator "0.1.4"]
      [im.chit/hara.time "2.4.8"]
-     [proto-repl-sayid "0.1.3"]]
+     [reinvdwoerd/sandbox "0.0.1"]]
   :main api
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
